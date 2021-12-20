d5fddeda41fedb086e5e875bb41559da52d5d188
Krasimir Koeff <kr@koeff.com>
2021-12-20 23:18:12 +0200

    wip: for in pods

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 parser = ArgumentParser()
 parser.add_argument('--context', help='Use specific kubeconfig context')
+parser.add_argument('--nodes', help='Look for pods in these nodes', required=False)
 args = parser.parse_args()
 
 
@@ -27,15 +28,27 @@ def getNmspNames(items):
 
 try:
 
-    config.load_kube_config()
     contexts, activeContext = config.list_kube_config_contexts()
-
     raiseExceptionIfFalse(checkIfContextExists(contexts,args.context),'Given Context does not exist.')
-
-    v1 = client.CoreV1Api()
+    # config.load_kube_config()
+    apiClient = config.new_client_from_config(context=args.context)
+    v1 = client.CoreV1Api(apiClient)
+    v1ext = client.AppsV1Api(apiClient)
     nmspData = v1.list_namespace().items
     namespaces = getNmspNames(nmspData)
+    pods = v1.list_pod_for_all_namespaces()
+    rsets = v1ext.list_replica_set_for_all_namespaces()
+    mydeployment = rsets.items[0].metadata.owner_references[0].name
+    deployments = v1ext.list_deployment_for_all_namespaces()
 
+    data = []
+    for eachPod in pods.items:
+        eachPodName = eachPod.metadata.name
+        eachPodParent = eachPod.metadata.owner_references[0].name
+        eachPodParentKind = eachPod.metadata.owner_references[0].kind
+
+
+        eachRset = rsets.items[0].metadata.owner_references[0].name
 
     print(namespaces)
 
